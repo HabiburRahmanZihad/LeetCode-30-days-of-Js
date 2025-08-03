75d200f8260fbe9e70aec10d9c45cfc964344a41
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-08-03 13:18:48 +0600

    Solve problem 30

---
+++ Problem-30.js
@@ -0,0 +1,96 @@
+//****Array Wrapper ****/
+/**
+ * Design a Calculator class. The class should provide the mathematical operations of addition, subtraction, multiplication, division, and exponentiation. It should also allow consecutive operations to be performed using method chaining. The Calculator class constructor should accept a number which serves as the initial value of result.
+
+Your Calculator class should have the following methods:
+
+add - This method adds the given number value to the result and returns the updated Calculator.
+subtract - This method subtracts the given number value from the result and returns the updated Calculator.
+multiply - This method multiplies the result  by the given number value and returns the updated Calculator.
+divide - This method divides the result by the given number value and returns the updated Calculator. If the passed value is 0, an error "Division by zero is not allowed" should be thrown.
+power - This method raises the result to the power of the given number value and returns the updated Calculator.
+getResult - This method returns the result.
+Solutions within 10-5 of the actual result are considered correct.
+ */
+
+class Calculator {
+    /**
+     * @param {number} value
+     */
+    constructor(value) {
+        if (typeof value !== 'number') {
+            throw new Error("Initial value must be a number");
+        }
+        this.result = value;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value) {
+        if (typeof value !== 'number') {
+            throw new Error("Value must be a number");
+        }
+        this.result += value;
+        return this;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value) {
+        if (typeof value !== 'number') {
+            throw new Error("Value must be a number");
+        }
+        this.result -= value;
+        return this;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    multiply(value) {
+        if (typeof value !== 'number') {
+            throw new Error("Value must be a number");
+        }
+        this.result *= value;
+        return this;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if (typeof value !== 'number') {
+            throw new Error("Value must be a number");
+        }
+        if (value === 0) {
+            throw new Error("Division by zero is not allowed");
+        }
+        this.result /= value;
+        return this;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        if (typeof value !== 'number') {
+            throw new Error("Value must be a number");
+        }
+        this.result **= value;
+        return this;
+    }
+
+    /**
+     * @return {number}
+     */
+    getResult() {
+        return this.result;
+    }
+}
